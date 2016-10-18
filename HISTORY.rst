a9b19c8d91538038c0e4a6b161bd33822ce9859a
Zack-- <zdibe@google.com>
2016-10-18 11:09:15 +0100

    Changelog for upcoming release 1.0.1

---
+++ HISTORY.rst
@@ -0,0 +1,12 @@
+.. :changelog:
+
+Release History
+---------------
+
+1.0.1 (2016-10-18)
++++++++++++++++++++
+
+**Bugfixes**
+
+- Fixed a bug when using ``buf``
+- Fixed a typo in docstring
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='vidhdr',
 
-    version='1.0',
+    version='1.0.1',
 
     description='Determine the type of a video',
     long_description=long_description,
